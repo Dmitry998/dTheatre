70d4130575eaa3e92d0704786aeeb32ef2b8ec62
Dmitry998 <40994829+Dmitry998@users.noreply.github.com>
2021-05-17 00:49:03 +0300

    commit

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
-if(isset($_COOKIE['token']))
-{
-	$token = $_COOKIE['token'];	
+if (isset($_COOKIE['token'])) {
+	$token = $_COOKIE['token'];
 	ConnectDB();
 	$res = $mysqli->query("SELECT * FROM users WHERE token = '$token'");
-	if($res->num_rows==1)
-	{
+	if ($res->num_rows == 1) {
 		$user = $res->fetch_object();
-		LogIn($user->id);		
+		LogIn($user->id);
 	}
 }
 
@@ -16,7 +14,7 @@ if(isset($_COOKIE['token']))
 function ConnectDB()
 {
 	global $mysqli;
-	$mysqli = new mysqli('localhost','root','12345678','Shoe_store');
+	$mysqli = new mysqli('localhost', 'root', '12345678', 'Shoe_store');
 }
 
 function LogIn($userid)
@@ -24,57 +22,80 @@ function LogIn($userid)
 	global $mysqli;
 	$_SESSION['userid'] = $userid;
 	$token = sprintf('%08x%08x%08x%08x', rand(), rand(), rand(), rand());
-	setcookie('token',$token,time()+3600*24*30);
+	setcookie('token', $token, time() + 3600 * 24 * 30);
 	$res = $mysqli->query("UPDATE users SET token = '$token' WHERE id = '$userid'");
 }
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset = "utf-8">
+	<meta charset="utf-8">
 	<title> Театр </title>
 	<link href="style.css" rel="stylesheet" type="text/css">
 </head>
+
 <body>
 	<div class="header">
 		<a class="logo" href="index.php"><img src="logoT.jpg" alt=""></a>
 		<div class="top-menu">
-		<ul>
-			<li><a href="index.php">Главная</a></li>
-			<li><a href="pages/actors.php">Актёры</a></li>
-			<li><a href="pages/spectacles.php">Спектакли</a></li>
-		<div class="auth">
-			<?php
-			if(isset($_SESSION['userid']))
-			{
-				$userid = $_SESSION['userid'];
-				$mysqli = new mysqli('localhost','root','12345678','theatre');
-				
-				$res = $mysqli->query("SELECT login From users WHERE id='$userid'");
-				$user = $res->fetch_object();
-				$login = $user->login;
+			<ul>
+				<li><a href="index.php">Главная</a></li>
+				<li><a href="pages/actors.php">Актёры</a></li>
+				<li><a href="pages/spectacles.php">Спектакли</a></li>
+				<div class="auth">
+					<?php
+					if (isset($_SESSION['userid'])) {
+						$userid = $_SESSION['userid'];
+						$mysqli = new mysqli('localhost', 'root', '12345678', 'theatre');
 
-				echo "<li><a href='basket.php'>Вы вошли как $login </a></li>
+						$res = $mysqli->query("SELECT login From users WHERE id='$userid'");
+						$user = $res->fetch_object();
+						$login = $user->login;
+
+						echo "<li><a href='basket.php'>Вы вошли как $login </a></li>
 				<li><a href='exit.php'>Выход</a></li>";
-			}
-			else
-			{
-				echo"<li><a href='authForm.php'>Вход</a></li>
+					} else {
+						echo "<li><a href='authForm.php'>Вход</a></li>
 				<li><a href='registrationForm.php'>Регистрация</a></li>";
-			}
-			?>
-		</ul>
-		</div>
+					}
+					?>
+			</ul>
 		</div>
 	</div>
+	</div>
 	<div class="content">
+		<?php
+		echo "<h2>О театре</h2>";
+		echo "<div class='about'><table><tr><td><img src='Files/theatreHouse.png' alt=''><br></td><td><P>Первый театральный сезон открылся 27 ноября 1993 премьерой спектакля «Приключения Буратино в Стране Дураков» на сцене ДК «Современник». Основа труппы театра — участники Народного театра юного зрителя при ДК «Современник» (руководитель Лидия Михайловна Ершова с 1971 г.). Собственное здание (бывший киноклуб «Дружба») театр получил в декабре 1995 г. и вселился в него, не ожидая реконструкции.</p></td></tr></table>
+
+		<p>В ноябре 2003 года завершились ремонт и реконструкция театра, в результате которой к основному зданию был сооружён пристрой, позволивший организовать закулисное пространство и получить подсобные помещения. Зрительный зал театра рассчитан на 81 место.</p>
+		
+		<p> Ежегодно театр выпускает 5-6 премьер, каждая из которых становится заметным событием в культурной жизни города. За 27 творческих сезонов было поставлено более 170 спектаклей и представлений для детей и взрослых.</p></div>";
+		echo "<h2>Все сеансы</h2>";
+		$res = $mysqli->query("SELECT sessions.id as id, DATE_FORMAT(sessions.date, '%d.%m.%Y %H:%i') as date, spectacles.name as name FROM sessions INNER JOIN spectacles ON spectacles.id=sessions.spectacle_id ORDER BY date");
+		echo "<div id= allSessions>";
+		echo "<table>";
+
+		echo "<tr><th>Спектакль</th><th>Дата сеанса</th></tr>";
+		$indexRow = 0;
+		while ($session =  $res->fetch_object()) {
+			$id = $session->id;
+			$name = $session->name;
+			$date = $session->date;
+			$photo = $actor->photo;
+			echo "<tr><td><a id='actor' href='pages/session.php?session_id=$id'>$name</a></td><td><a id='actor' href='pages/session.php?session_id=$id'>$date</a></td></tr>";
+		}
+		echo "</tr></table></div>";
+		?>
 		<div class='mainPage'>
 		</div>
 	</div>
 	<div class="footer">
-			8(960)319-71-39 г. Заречный Пензенская область<br>
-			Горячая линия
+		8(960)319-71-39 г. Заречный Пензенская область<br>
+		Горячая линия
 	</div>
 </body>
+
 </html>
--- pages/session.php
+++ pages/session.php
@@ -5,6 +5,12 @@ global $login;
 $login = null;
 global $reservationPlaces;
 $reservationPlaces = array();
+global $price1;
+$price1 = 0;
+global $price2;
+$price2 = 0;
+global $price3;
+$price3 = 0;
 ?>
 
 <!DOCTYPE html>
@@ -56,12 +62,40 @@ $reservationPlaces = array();
         INNER JOIN spectacles ON spectacles.id=sessions.spectacle_id WHERE sessions.id = $session_id");
 
         $session =  $res->fetch_object();
+        
         echo "<div class='cinoteatr'>";
         echo "<h2>$session->spectacle_name</h2>";
         echo "<h2>Дата и время сеанса: $session->date</h2>";
+
+        $res = $mysqli->query("SELECT price FROM places WHERE number=1 AND session_id=$session_id");
+        $price1 = $res->fetch_object();
+        $price1 = $price1->price;
+
+        $res = $mysqli->query("SELECT price FROM places WHERE number=28 AND session_id=$session_id");
+        $price2 = $res->fetch_object();
+        $price2 = $price2->price;
+
+        $res = $mysqli->query("SELECT price FROM places WHERE number=81 AND session_id=$session_id");
+        $price3 = $res->fetch_object();
+        $price3 = $price3->price;
+
+        echo "<div class='legend'><table>
+        <tr><td><div class='mestaExample'></div></td>
+        <td>Сектор А цена: $price1</td><tr>
+        <tr><td><div class='mestaExample'></div></td>
+        <td>Сектор В цена: $price2</td><tr>
+        <tr><td><div class='mestaExample'></div></td>
+        <td>Сектор С цена: $price3</td><tr>
+        <tr><td><div class='mestaExample'></div></td>
+        <td>Выбранные места</td><tr>
+        <tr><td><div class='mestaExample'></div></td>
+        <td>Занятые места</td><tr>    
+        </table></div>";
         if ($login != null) {
             $userid = $_SESSION['userid'];
             echo "<p><button onclick=\"GetChoosenPlaces('../reservationTicket.php',$session_id,$userid)\">Забронировать</button></p>";
+        } else {
+            echo "<h2>Только зарегистрированные пользователи могут бронировать билеты</h2>";
         }
 
         $res = $mysqli->query("SELECT places.number as number FROM places INNER JOIN tickets ON tickets.place_id=places.id WHERE places.session_id=$session_id");
@@ -69,13 +103,13 @@ $reservationPlaces = array();
             array_push($reservationPlaces, (int)$places->number);
         }
         echo "<div id='count_'></div>";
+        echo "<div id='status'></div>";
         $row = 1;
-        for($i=1; $i < 82; $i++) {
-            if($i % 9 == 0){
-                echo "<div class='mesta'>$i</div>";  
+        for ($i = 1; $i < 82; $i++) {
+            if ($i % 9 == 0) {
+                echo "<div class='mesta'>$i</div>";
                 $row++;
-            }
-            else {
+            } else {
                 echo "<div class='mesta'>$i</div>";
             }
         }
@@ -84,7 +118,7 @@ $reservationPlaces = array();
         <div id="AjaxDiv"></div>
     </div>
     <div class='mainPage'>
-	</div>
+    </div>
     <div class="footer">
         8(960)319-71-39 г. Заречный Пензенская область<br>
         Горячая линия
@@ -93,16 +127,36 @@ $reservationPlaces = array();
 
 
 <script>
+    var exmaplePlaces = document.getElementsByClassName('mestaExample');
+    for (i = 0; i < exmaplePlaces.length; i++) {
+        switch (i) {
+            case 0:
+                exmaplePlaces[i].style.background = 'green';
+                break;
+            case 1:
+                exmaplePlaces[i].style.background = 'yellow';
+                break;
+            case 2:
+                exmaplePlaces[i].style.background = 'orange';
+                break;
+            case 3:
+                exmaplePlaces[i].style.background = 'red';
+                break;
+            case 4:
+                exmaplePlaces[i].style.background = 'black';
+                break;
+        }
+    }
     var l = document.getElementsByClassName('mesta');
 
     for (let i = 0; i < l.length; i++) {
-        if(l[i].innerHTML < 28){
+        if (l[i].innerHTML < 28) {
             l[i].style.background = 'green';
         }
-        if(l[i].innerHTML > 27 && l[i].innerHTML < 55){
+        if (l[i].innerHTML > 27 && l[i].innerHTML < 55) {
             l[i].style.background = 'yellow';
         }
-        if(l[i].innerHTML > 54){
+        if (l[i].innerHTML > 54) {
             l[i].style.background = 'orange';
         }
     }
@@ -126,6 +180,15 @@ $reservationPlaces = array();
         return z_mest;
     }
 
+    function getSum(){
+        var sum = 0;
+        for (let i = 0; i < l.length; i++) {
+            if (l[i].style.background == 'red') {
+                sum += getPriceForPlace(Number(l[i].innerHTML));
+            }
+        }
+        return sum;
+    }
     function GetPlaces() {
         var places = [];
         for (let i = 0; i < l.length; i++) {
@@ -138,27 +201,47 @@ $reservationPlaces = array();
     for (let i = 0; i < l.length; i++) {
         l[i].onclick = function() {
             let background = this.style.background;
-            if (background != 'black') {
-                if (background == 'red') {
-                    if(l[i].innerHTML < 28){
-                        this.style.background = 'green';
+            if (<?php echo json_encode($login); ?>) {
+                if (background != 'black') {
+                    if (background == 'red') {
+                        if (l[i].innerHTML < 28) {
+                            this.style.background = 'green';
+                        }
+                        if (l[i].innerHTML > 27 && l[i].innerHTML < 55) {
+                            this.style.background = 'yellow';
+                        }
+                        if (l[i].innerHTML > 54) {
+                            this.style.background = 'orange';
+                        }
+
+                    } else {
+                        this.style.background = 'red';
                     }
-                    if(l[i].innerHTML > 27 && l[i].innerHTML < 55){
-                        this.style.background = 'yellow';
-                    }
-                    if(l[i].innerHTML > 54){
-                        this.style.background = 'orange';
-                    }
-                    
-                } else {
-                    this.style.background = 'red';
                 }
+            } else {
+                document.getElementById('status').innerHTML = 'Для бронирования мест необходимо авторизоваться!';
             }
-            document.getElementById('count_').innerHTML = 'Выбрано мест : ' + Count_Mest();
+            if(Count_Mest() > 0){
+                document.getElementById('count_').innerHTML = 'Выбрано мест : ' + Count_Mest() + ' На сумму: ' + getSum() + ' рублей';
+            } else{
+                document.getElementById('count_').innerHTML = '';
+            }
         }
     }
 
-
+    function getPriceForPlace(placeNumber) {
+        var price1 = Number(JSON.parse('<?php echo json_encode($price1); ?>'));
+        var price2 = Number(JSON.parse('<?php echo json_encode($price2); ?>'));
+        var price3 = Number(JSON.parse('<?php echo json_encode($price3); ?>'));
+        if(placeNumber < 28){
+            return price1;
+        }
+        if(placeNumber > 28 && placeNumber < 55){
+            return price2;
+        }
+        return price3;
+    }
+    
     function GetChoosenPlaces(url, session, userId) {
         var httpRequest = new XMLHttpRequest();
         httpRequest.onreadystatechange = function() {
@@ -185,10 +268,9 @@ $reservationPlaces = array();
     function alertResponse(httpRequest) {
         if (httpRequest.readyState == 4) {
             if (httpRequest.status == 200) {
-                alert('Ответ получен');
-                var responseDiv = document.getElementById('AjaxDiv');
+                var responseDiv = document.getElementById('status');
                 var reservPlaces = JSON.parse(httpRequest.responseText);
-                if(reservPlaces.length > 1){
+                if (Count_Mest() > 1) {
                     responseDiv.innerHTML = "Места успешно забронированы";
                 } else {
                     responseDiv.innerHTML = "Место успешно забронировано";
